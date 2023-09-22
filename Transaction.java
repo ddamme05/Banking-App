199b2446573d98736c222651030a05b58a14c5cd
ddamme05 <ddamme05@gmail.com>
2023-09-21 22:31:31 -0400

    Added transaction support.

--- Transaction.java
+++ Transaction.java
@@ -1,2 +1,19 @@
 public class Transaction {
+    //Using enum for readability and security
+    public enum Type {DEPOSIT, WITHDRAW}
+    private double amount;
+    private Type type;
+
+    public Transaction(double amount, Type type) {
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Type getType() {
+        return type;
+    }
 }
