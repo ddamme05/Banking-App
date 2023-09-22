b679910fd559bdeaa34d1a45f692762a3ed13dcf
ddamme05 <ddamme05@gmail.com>
2023-09-21 22:10:45 -0400

    Added basic account functions.

--- Account.java
+++ Account.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+    private String accountId;
+    private double balance;
+    //Initialized as a list for polymorphism, I may make it LinkedList in the future
+    private List<Transaction> transactions;
+
+    public Account(String accountId) {
+        this.accountId = accountId;
+        this.balance = 0.0;
+        this.transactions = new ArrayList<>();
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
