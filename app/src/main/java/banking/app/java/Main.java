122f4eb9b06988e067f157960e22a9569f897974
ddamme05 <ddamme05@gmail.com>
2023-09-22 22:39:01 -0400

    Refactored Main to make it easily testable.

--- app/src/main/java/banking/app/java/Main.java
+++ app/src/main/java/banking/app/java/Main.java
@@ -7,27 +7,14 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
+        executeCapitalOneFlow();
+        executeTDBankFlow();
+    }
+
+    public static void executeCapitalOneFlow() {
         Bank capital_one = new Bank();
         QueryHandler capitalOneHandler = new QueryHandler(capital_one);
-
-        List<String[]> capitalOneQueries = new ArrayList<>();
-        capitalOneQueries.add(new String[]{"CREATE_ACCOUNT", "1", "account3"});
-        capitalOneQueries.add(new String[]{"CREATE_ACCOUNT", "2", "account2"});
-        capitalOneQueries.add(new String[]{"CREATE_ACCOUNT", "3", "account1"});
-        capitalOneQueries.add(new String[]{"DEPOSIT", "4", "account1", "2000"});
-        capitalOneQueries.add(new String[]{"DEPOSIT", "5", "account2", "3000"});
-        capitalOneQueries.add(new String[]{"DEPOSIT", "6", "account3", "4000"});
-        capitalOneQueries.add(new String[]{"TOP_SPENDERS", "4"});
-        capitalOneQueries.add(new String[]{"TRANSFER", "7", "account3", "account2", "500"});
-        capitalOneQueries.add(new String[]{"TRANSFER", "8", "account3", "account1", "1000"});
-        capitalOneQueries.add(new String[]{"TRANSFER", "9", "account1", "account2", "2500"});
-        capitalOneQueries.add(new String[]{"TOP_SPENDERS", "3"});
-        capitalOneQueries.add(new String[]{"TRANSFER", "10", "account1", "account2", "500"});
-        capitalOneQueries.add(new String[]{"CASHBACK", "account1"});
-        capitalOneQueries.add(new String[]{"MERGE_ACCOUNTS", "account1", "account2"});
-
-
-        List<String> capitalOneResults = capitalOneHandler.handleQueries(capitalOneQueries);
+        List<String> capitalOneResults = executeCapitalOneQueries(capitalOneHandler);
 
         for (String result : capitalOneResults) {
             System.out.println(result);
@@ -35,23 +22,32 @@ public class Main {
 
         System.out.println("\nStatus of all accounts at Capital One:");
         printAccountStatus(capital_one);
+    }
 
+    public static List<String> executeCapitalOneQueries(QueryHandler handler) {
+        List<String[]> queries = new ArrayList<>();
+        queries.add(new String[]{"CREATE_ACCOUNT", "1", "account3"});
+        queries.add(new String[]{"CREATE_ACCOUNT", "2", "account2"});
+        queries.add(new String[]{"CREATE_ACCOUNT", "3", "account1"});
+        queries.add(new String[]{"DEPOSIT", "4", "account1", "2000"});
+        queries.add(new String[]{"DEPOSIT", "5", "account2", "3000"});
+        queries.add(new String[]{"DEPOSIT", "6", "account3", "4000"});
+        queries.add(new String[]{"TOP_SPENDERS", "4"});
+        queries.add(new String[]{"TRANSFER", "7", "account3", "account2", "500"});
+        queries.add(new String[]{"TRANSFER", "8", "account3", "account1", "1000"});
+        queries.add(new String[]{"TRANSFER", "9", "account1", "account2", "2500"});
+        queries.add(new String[]{"TOP_SPENDERS", "3"});
+        queries.add(new String[]{"TRANSFER", "10", "account1", "account2", "500"});
+        queries.add(new String[]{"CASHBACK", "account1"});
+        queries.add(new String[]{"MERGE_ACCOUNTS", "account1", "account2"});
+
+        return handler.handleQueries(queries);
+    }
+
+    public static void executeTDBankFlow() {
         Bank td_bank = new Bank();
         QueryHandler tdBankHandler = new QueryHandler(td_bank);
-
-        List<String[]> tdBankQueries = new ArrayList<>();
-        tdBankQueries.add(new String[]{"CREATE_ACCOUNT", "1", "account3"});
-        tdBankQueries.add(new String[]{"CREATE_ACCOUNT", "2", "account2"});
-        tdBankQueries.add(new String[]{"CREATE_ACCOUNT", "3", "account1"});
-        tdBankQueries.add(new String[]{"DEPOSIT", "4", "account1", "2000"});
-        tdBankQueries.add(new String[]{"DEPOSIT", "5", "account2", "3000"});
-        tdBankQueries.add(new String[]{"DEPOSIT", "6", "account3", "4000"});
-        tdBankQueries.add(new String[]{"TRANSFER", "8", "account3", "account2", "500"});
-        tdBankQueries.add(new String[]{"TRANSFER", "9", "account3", "account1", "1000"});
-        tdBankQueries.add(new String[]{"TRANSFER", "10", "account1", "account2", "2500"});
-
-
-        List<String> tdBankResults = tdBankHandler.handleQueries(tdBankQueries);
+        List<String> tdBankResults = executeTDBankQueries(tdBankHandler);
 
         for (String result : tdBankResults) {
             System.out.println(result);
@@ -59,6 +55,21 @@ public class Main {
 
         System.out.println("\nStatus of all accounts at TD Bank:");
         printAccountStatus(td_bank);
+    }
+
+    public static List<String> executeTDBankQueries(QueryHandler handler) {
+        List<String[]> queries = new ArrayList<>();
+        queries.add(new String[]{"CREATE_ACCOUNT", "1", "account3"});
+        queries.add(new String[]{"CREATE_ACCOUNT", "2", "account2"});
+        queries.add(new String[]{"CREATE_ACCOUNT", "3", "account1"});
+        queries.add(new String[]{"DEPOSIT", "4", "account1", "2000"});
+        queries.add(new String[]{"DEPOSIT", "5", "account2", "3000"});
+        queries.add(new String[]{"DEPOSIT", "6", "account3", "4000"});
+        queries.add(new String[]{"TRANSFER", "8", "account3", "account2", "500"});
+        queries.add(new String[]{"TRANSFER", "9", "account3", "account1", "1000"});
+        queries.add(new String[]{"TRANSFER", "10", "account1", "account2", "2500"});
+
+        return handler.handleQueries(queries);
     }
 
     private static void printAccountStatus(Bank bank) {
